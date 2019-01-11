7b9d92e6e979d284f28e5207ce3f4753d2089aea
jigunho <jce0831@gmail.com>
2019-01-11 23:05:55 +0900

    banner

--- rescat/rescat/Main/VC/FundingHome/MainViewController.swift
+++ rescat/rescat/Main/VC/FundingHome/MainViewController.swift
@@ -52,6 +52,7 @@ class MainViewController: UIViewController , AACarouselDelegate , APIServiceCall
         request.requestFundingBannerList(0); request.requestFundingBannerList(1);
         request.requestFundingBannerList(2)
 
+        titleArray.removeAll()
         fundingBannerList.removeAll(); photoArray.removeAll()
     }
     override func viewDidLoad() {
@@ -133,18 +134,6 @@ class MainViewController: UIViewController , AACarouselDelegate , APIServiceCall
 //        })
     }
 
-    
-
-    //optional method (interaction for touch image)
-//    func didSelectCarouselView(_ view: AACarousel ,_ index: Int) {
-//
-//        let alert = UIAlertView.init(title:"Alert" , message: titleArray[index], delegate: self, cancelButtonTitle: "OK")
-//        alert.show()
-//
-//        //startAutoScroll()
-//        //stopAutoScroll()
-//    }
-//
     //optional method (show first image faster during downloading of all images)
     func callBackFirstDisplayView(_ imageView: UIImageView, _ url: [String], _ index: Int) {
         imageView.kf.setImage(with: URL(string: url[index]))
@@ -164,19 +153,7 @@ class MainViewController: UIViewController , AACarouselDelegate , APIServiceCall
         //optional method
         reviewImage.stopScrollImageView()
     }
-    func didSelectCarouselView(_ view: AACarousel, _ index: Int) {
-        
-    }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+  
     @IBAction func unwindToHomeScreen(segue: UIStoryboardSegue) {
         print("Unwind segue to home screen triggered!")
     }
@@ -265,6 +242,15 @@ extension MainViewController : UITableViewDelegate, UITableViewDataSource {
         }
 
     }
+    func didSelectCarouselView(_ view: AACarousel ,_ index: Int) {
+        
+        guard let url = URL(string: titleArray[index]) else { return }
+        UIApplication.shared.open(url)
+
+        //startAutoScroll()
+        //stopAutoScroll()
+    }
+
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         if indexPath.row == 2 {
             return CGFloat(170)
@@ -300,6 +286,7 @@ extension MainViewController {
 //            print("fundingBannerList model len \(fundingBannerList.count)")
             for i in 0..<fundingBannerList.count {
                 photoArray.append(gsno(fundingBannerList[i].photoUrl))
+                titleArray.append(gsno(fundingBannerList[i].link))
             }
 
             reviewImage.setCarouselData(paths: photoArray,  describedTitle: [], isAutoScroll: true, timer: 5.0, defaultImage: "https://rescat.s3.ap-northeast-2.amazonaws.com/static/1547213699228_banner.png")
